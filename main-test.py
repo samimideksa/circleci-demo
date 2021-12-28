6bafd8618f7d9c0caffce75ce09804edef0917a7
Samuel Degife <samuel@Samuels-MacBook-Pro.local>
2021-12-28 16:31:08 -0600

    Initial commit

---
+++ main-test.py
@@ -0,0 +1,8 @@
+from main import Add
+
+def testAdd():
+    assert Add(2, 3) == 5
+    print ("Add function works correctly.")
+
+if __name__ == '__main__':
+    testAdd()
---
+++ main.py
@@ -0,0 +1,10 @@
+def Add(a, b):
+    return a + b
+
+def sayHello():
+    print ("Sup world from source make")
+
+if __name__ == '__main__':
+    sayHello()
+
+
