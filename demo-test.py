ecb1bff09039eb5f965c5ff3d9dcec8c1d5e5212
Samuel Degife <samuel@Samuels-MacBook-Pro.local>
2021-12-28 20:33:46 -0600

    random change

--- demo-test.py
+++ demo-test.py
@@ -2,7 +2,7 @@ from demo import substract
 
 def testSubstract():
     assert substract(5, 3) == 2
-    assert substract(10, 5) == 3
+    assert substract(10, 5) == 5
     print ("Add function works correctly.")
 
 if __name__ == '__main__':
